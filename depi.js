1934e183e9d7e686a2ba6113767585e6d959e3da
OMARTOHAMY <omartohamy.work@gmail.com>
2024-12-06 20:41:38 +0200

    v5

--- depi.js
+++
@@ -1,6 +0,0 @@
-let result = document.querySelector('#result');
-let text = document.querySelector('#text');
-
-function addText() {
-    result.textContent += text.value;
-}
--- test.js
+++ test.js
@@ -17,38 +17,4 @@ function modeChange() {
     }
 };
 
-// function openElement(Name){
-//   window.location.href = 'country.html';
-//   arrElements.forEach(element => {
-//       if (element.name === Name) {
-//           info.innerHTML += `
-//               <img src=${element.flags.png} alt="flag">
-//               <div class="details">
-//                   <div class="top">
-//                       <div class="left">
-//                           <h2>${element.name}</h2>
-//                           <h4><span>Native Name:</span>${element.nativeName}</h4>
-//                           <h4><span>Population:</span>${element.population}</h4>
-//                           <h4><span>Region:</span>${element.region}</h4>
-//                           <h4><span>Sub Region:</span>${element.subregion}</h4>
-//                           <h4><span>Capital:</span>${element.capital}</h4>
-//                       </div>
-//                       <div class="right">
-//                           <h4><span>Top Level Domain:</span>${element.topLevelDomain}</h4>
-//                           <h4><span>Currency:</span>${element.currencies[0].name}</h4>
-//                           <h4><span>Languages:</span>${element.languages.map(lang => lang.name).join(', ')}</h4>
-//                       </div>
-//                   </div>
-//                   <div class="bottom">
-//                       <div class="border-countries">
-//                           <h3>Border Countries:</h3>
-//                           <div class="btns">
-//                           ${element.borders.map(border => `<button class='btnBorder' >${border}</button>`).join('')}
-//                           </div>
-//                       </div>
-//                   </div>
-//               </div>
-//           `;
-//       }
-//   });
-// }
+export default info;
